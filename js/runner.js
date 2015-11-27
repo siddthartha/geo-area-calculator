980add9a2b956f26deae525aaad9910910085acb
Anton Sadovnikoff <sadovnikoff@gmail.com>
2015-11-27 20:33:59 +0300

    fix directory structure, add package.json

--- js/runner.js
+++
@@ -1,5 +0,0 @@
-var runner = require("./node_modules/qunit");
-runner.run({
-    code : "geoarea.js",
-    tests : "tests.js"
-});
---
+++ qunit-runner.js
@@ -0,0 +1,5 @@
+var runner = require("./node_modules/qunit");
+runner.run({
+    code : "js/geoarea.js",
+    tests : "tests/qunit/tests.js"
+});
