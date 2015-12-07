e06bdc3cd14694fc3a89f64662937fed00e3b2bf
Anton Sadovnikoff <sadovnikoff@gmail.com>
2015-12-07 11:07:27 +0300

    fix JS version and tests update

--- js/geoarea.js
+++ js/geoarea.js
@@ -22,12 +22,12 @@
 
     // инициализация эквивалентной сферы
     function initSph(a, f) {
-        b = a * (1. - f);
-        e2 = f * (2. - f);
-        R_auth = b * Math.sqrt(1. + powSeries(e2, 2. / 3., 3. / 5., 4. / 7.));
-        to_auth_2 = powSeries(e2, -1. / 3., -31. / 180., -59. / 560.);
-        to_auth_4 = powSeries(e2, 0., 17. / 360., 61. / 1260.);
-        to_auth_6 = powSeries(e2, 0., 0., -383. / 45360.);
+        var b = a * (1. - f);
+        var e2 = f * (2. - f);
+        var R_auth = b * Math.sqrt(1. + powSeries(e2, 2. / 3., 3. / 5., 4. / 7.));
+        var to_auth_2 = powSeries(e2, -1. / 3., -31. / 180., -59. / 560.);
+        var to_auth_4 = powSeries(e2, 0., 17. / 360., 61. / 1260.);
+        var to_auth_6 = powSeries(e2, 0., 0., -383. / 45360.);
         return [R_auth, to_auth_2, to_auth_4, to_auth_6];
     }
 
@@ -53,17 +53,17 @@
     }
 
     function rotate(x, y, a) {
-        c = Math.cos(a);
-        s = Math.sin(a);
-        u = x * c + y * s;
-        v = -x * s + y * c;
+        var c = Math.cos(a);
+        var s = Math.sin(a);
+        var u = x * c + y * s;
+        var v = -x * s + y * c;
         return [u, v];
     }
 
 
     function inverse(lat1, lon1, lat2, lon2) {
         var z, y, z;
-        tmp = spherToCart(lat2, lon2);
+        var tmp = spherToCart(lat2, lon2);
         x = tmp[0];
         y = tmp[1];
         z = tmp[2];
@@ -74,18 +74,18 @@
         z = tmp[0];
         x = tmp[1];
         c = cartToSpher(x, y, z);
-        lat = c[0];
-        lon = c[1];
-        dist = Math.PI / 2 - lat;
-        azi = Math.PI - lon;
+        var lat = c[0];
+        var lon = c[1];
+        var dist = Math.PI / 2 - lat;
+        var azi = Math.PI - lon;
         return [dist, azi];
     }
 
     function direct(lat1, lon1, dist, azi) {
-        tmp = spherToCart(Math.PI / 2 - dist, Math.PI - azi);
-        x = tmp[0];
-        y = tmp[1];
-        z = tmp[2];
+        var tmp = spherToCart(Math.PI / 2 - dist, Math.PI - azi);
+        var x = tmp[0];
+        var y = tmp[1];
+        var z = tmp[2];
 
         tmp = rotate(z, x, lat1 - Math.PI / 2);
         z = tmp[0];
@@ -95,27 +95,29 @@
         x = tmp[0];
         y = tmp[1];
 
-        c = cartToSpher(x, y, z);
-        lat2 = c[0];
-        lon2 = c[1];
+        var c = cartToSpher(x, y, z);
+        var lat2 = c[0];
+        var lon2 = c[1];
         return [lat2, lon2];
     }
 
     function angular(lat1, lon1, lat2, lon2, azi13, azi23) {
-        failure = false;
-        c1 = inverse(lat2, lon2, lat1, lon1);
-        dist12 = c1[0];
-        azi21 = c1[1];
+        var failure = false;
+        var c1 = inverse(lat2, lon2, lat1, lon1);
+        var dist12 = c1[0];
+        var azi21 = c1[1];
 
-        c2 = inverse(lat1, lon1, lat2, lon2);
+        var c2 = inverse(lat1, lon1, lat2, lon2);
         dist12 = c2[0];
-        azi12 = c2[1];
-        cos_beta1 = Math.cos(azi13 - azi12);
-        sin_beta1 = Math.sin(azi13 - azi12);
-        cos_beta2 = Math.cos(azi21 - azi23);
-        sin_beta2 = Math.sin(azi21 - azi23);
-        cos_dist12 = Math.cos(dist12);
-        sin_dist12 = Math.sin(dist12);
+        var azi12 = c2[1];
+        var cos_beta1 = Math.cos(azi13 - azi12);
+        var sin_beta1 = Math.sin(azi13 - azi12);
+        var cos_beta2 = Math.cos(azi21 - azi23);
+        var sin_beta2 = Math.sin(azi21 - azi23);
+        var cos_dist12 = Math.cos(dist12);
+        var sin_dist12 = Math.sin(dist12);
+	var lat3,lon3, dist13;
+
         if (sin_beta1 === 0. && sin_beta2 === 0.) {
             failure = true;
             lat3 = 0.;
@@ -136,7 +138,7 @@
             }
         } else {
             dist13 = Math.atan2(Math.fabs(sin_beta2) * sin_dist12, cos_beta2 * Math.fabs(sin_beta1) + Math.fabs(sin_beta2) * cos_beta1 * cos_dist12);
-            c3 = direct(lat1, lon1, dist13, azi13);
+            var c3 = direct(lat1, lon1, dist13, azi13);
             lat3 = c3[0];
             lon3 = c3[1];
 
@@ -192,7 +194,9 @@
         var to_auth_6 = eSph[3];
 
         var tau = 0.;
-
+        var lat1 = 0.;
+        var lon1 = 0.;
+        
         for(var i= 1; i<=polygon.length; i++) {
             // прочитать долготу и широту
             var lat = deg2rad(polygon[i-1][0]);
@@ -225,6 +229,8 @@
             }
             lon1 = lon;
             lat1 = lat;
+            // some debug
+            // console.log(JSON.stringify([ lat1, lon1 ]));
         }
 
         // вычислить поворот в 1-й вершине
